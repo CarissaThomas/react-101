5e74f08016345bc08a06059f8b34bcb45c5ce400
Carissa Illy <>
2023-07-18 15:39:17 -0700

    style the button and form piece

--- meme-generator/src/Header.js
+++ meme-generator/src/Header.js
@@ -3,12 +3,12 @@ import troll from './images/Troll Face.png';
 
 function Header() {
   return (
-      <div className="header">
-          <div className="left-side">
-            <img src={troll}></img>
-              <h1>Meme Generator</h1>
-        </div>
-            <h3>React Course - Project</h3>
+    <div className="header">
+      <div className="left-side">
+        <img src={troll}></img>
+        <h1>Meme Generator</h1>
+      </div>
+      <h3>React Course - Project</h3>
     </div>
   );
 }
