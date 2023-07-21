b0ed17089a1652d1ccaca1327345c50fbf91ed1a
Carissa Illy <>
2023-07-21 13:12:40 -0700

    Meme image generator working

--- meme-generator/src/Meme.js
+++ meme-generator/src/Meme.js
@@ -1,15 +1,38 @@
 import './Meme.css';
+import memeData from './memeData';
+import React from 'react';
 
-function Meme() {
+export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+  
+
+  function displayMeme() {
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    console.log(memesArray[randomNumber].url);
+
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <main>
-      <form className="form">
-        <input type="text" className="form-input" text="top" />
-        <input type="text" className="form-input" text="bottom" />
-        <button className="form-button">Get a new meme image</button>
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" className="form-input" defaultValue="Shut up" />
+        <input
+          type="text"
+          className="form-input"
+          defaultValue="and take my money"
+        />
+        <button className="form-button" onClick={displayMeme}>
+          Get a new meme image
+        </button>
       </form>
+      <img className="memeImage" src={memeImage} />
     </main>
   );
 }
 
-export default Meme;
