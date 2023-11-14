98dfbb244d0e0c70b602ef2c5c81d27ade9319f4
Carissa Illy <>
2023-11-14 15:25:22 -0800

    home page complete

--- react-routing/van-life/src/home.js
+++ react-routing/van-life/src/home.js
@@ -1,8 +1,12 @@
-import React    from "react";
+import React from "react";
+import "./home.css"
+
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div class="home-container">
+            <h1>You got the travel plans, we got the travel vans</h1>
+            <p>Add adventure to you life by joining the #vanlife movement.  Rent the perfect van to make your perfect road trip.</p>
+            <button>Find your van</button>
       </div>
   );
 }
