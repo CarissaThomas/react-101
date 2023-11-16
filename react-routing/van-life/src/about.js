7a18062fe13e4963bdfb6b8a007ea01f1f97a191
Carissa Illy <>
2023-11-16 11:58:03 -0800

    base CSS complete

--- react-routing/van-life/src/about.js
+++ react-routing/van-life/src/about.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import './about.css';
+
 function About() {
   return (
-    <div>
-      <h1>Don't squeeze in a sedan when you could relax in a van.</h1>
-      <p>
-        Our mission is to blah blah blah blah blah blah blah blah blah blah blah
-        blah blah blah blah blah{' '}
-      </p>
-      <p>And then lolololololololololololol </p>
+    <div class="about-container">
+      <div >
+        <h1>Don't squeeze in a sedan when you could relax in a van.</h1>
+        <p>
+          Our mission is to blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah{' '}
+        </p>
+        <p>And then lolololololololololololol </p>
+      </div>
 
-      <div class="explore-div">
-        <h2>Your destination is waiting</h2>
-        <h2>Your van is ready</h2>
+      <div class="explore-container">
+        <h3>Your destination is waiting</h3>
+        <h3>Your van is ready</h3>
         <button>Explore our vans</button>
       </div>
     </div>
